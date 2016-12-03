04f53f9225c810c8478ce3ec40ce8fa76eeaaa19
Carlosg19 <carlos.garcia@ttu.edu>
2016-12-03 15:49:04 -0600

    added Snaketutorial

---
+++ SnakeTutorial/Assets/MyFirstScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyFirstScript : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject obj;
+    public Transform trans;
+    public BoxCollider boxcoll;
+    public MyFirstScript myScript;
+    public Light sceneLight;
+
+
+
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        sceneLight.intensity -= 0.005f;
+	
+	}
+
+
+
+}
---
+++ SnakeTutorial/Assets/Script/Control.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour
+{
+
+    Transform SnakeTransform;           //Transform  -> Position Control
+    float lastmove;                     //Time control
+    float TimeInBetweenMoves = 0.25f;   //Time control
+    Vector3 direction;                  //Postion -> Direction
+
+    int[,] location = new int[10,10];
+    int SnakeScore = 5;
+    int snakeX = 0;
+    int snakeY = 4;
+
+    bool lost;
+
+
+    private void Start()
+    {
+        SnakeTransform = GetComponent<Transform>();
+        direction = Vector3.right;
+
+        location[snakeX, snakeY] = SnakeScore;
+
+    }
+
+
+	private void Update()
+    {
+        if (lost)
+        {
+            return;
+        }
+
+
+
+        if(Time.time - lastmove > TimeInBetweenMoves)
+        {
+
+
+            //Reset Tiles that are 1
+            for(int i = 0; i < location.GetLength(0); i++)
+            {
+                for (int j = 0; j < location.GetLength(1); j++)
+                {
+                    if(location[i,j] > 0)
+                    {
+                        location[i, j]--;
+                        if(location[i,j] == 0)
+                        {
+                            //destroy something
+                            GameObject tail = GameObject.Find(i.ToString() + j.ToString());
+                            if (tail != null)
+                            {
+                                Destroy(tail);
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+            lastmove = Time.time;
+
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.transform.position = new Vector3(snakeX, snakeY, 0);
+            go.name = snakeX.ToString() + snakeY.ToString();
+
+            //Limits
+
+            if(direction.x == 1)
+            {
+                snakeX++;
+            }
+            if (direction.x == -1)
+            {
+                snakeX--;
+            }
+            if (direction.y == 1)
+            {
+                snakeY++;
+            }
+            if (direction.y == -1)
+            {
+                snakeY--;
+            }
+
+            if (snakeX > 9 || snakeX < 0 || snakeY > 9 || snakeY < 0)
+            {
+                lost = true;
+                Debug.Log("You Lost");
+                
+            }
+            else
+            {
+                //we eat an apple
+                if (location[snakeX, snakeY] == -1)
+                {
+                    SnakeScore++;
+
+                    //Create Apple
+                }
+                else if (location[snakeX, snakeY] != 0)
+                {
+                    lost = true;
+                    Debug.Log("You lost");
+                    return;
+                }
+
+                location[snakeX, snakeY] = SnakeScore;
+                SnakeTransform.position += direction;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+
+    }
+}
