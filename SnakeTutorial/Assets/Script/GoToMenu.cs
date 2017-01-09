a5679e4218a46965f89539344bc92e315339560c
Carlosg19 <carlos.garcia@ttu.edu>
2017-01-08 23:26:03 -0600

    SnakeTutorial

---
+++ SnakeTutorial/Assets/Script/GoToMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToMenu : MonoBehaviour {
+
+    public void Click()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+}
---
+++ SnakeTutorial/Assets/Script/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	
+    public void Click()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+
+}
